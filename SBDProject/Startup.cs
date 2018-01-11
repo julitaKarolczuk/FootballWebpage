a2a89504bff9cec11ca4661e6367679467ef3f24
Krzysztof Kiryłowicz <krzysztof.kirylowicz@gmail.com>
2018-01-11 16:17:43 +0100

    Add project files.

---
+++ SBDProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SBDProject.Startup))]
+namespace SBDProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
